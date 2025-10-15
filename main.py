0441f41cb304cc4914ad3f54d868d8d261c3f0f1
Des Whelan <wheland6@tcd.ie>
2025-10-15 11:34:12 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# from turtle import Turtle, Screen
+#
+# donatello = Turtle()
+#
+# my_screen = Screen()
+# my_screen.canvheight
+# print(my_screen.canvwidth)
+# donatello.shape("turtle")
+# donatello.color("purple")
+#
+# donatello.forward(500)
+#
+# my_screen.exitonclick()
+#
+# print(donatello)
+
+from prettytable import PrettyTable
+
+my_table = PrettyTable()
+
+my_table.add_column("Name", ["Schmeichel", "Neville", "Stam", "Ferdinand", "Irwin", "Ronaldo", "Keane", "Scholes", "Giggs", "Cantona", "Rooney"])
+
+my_table.add_column("Position", ["GK", "RB", "CB", "CB", "LB", "RW", "CM", "CM", "LM", "ST", "ST"])
+
+my_table.add_column("Number", [1, 2, 6, 5, 3, 7, 16, 18, 11, 7, 10])
+
+# my_table.align = "l"
+my_table.align["Name"] = "l"
+my_table.header_style = "upper"
+
+print(my_table.align)
+
+print(my_table)
